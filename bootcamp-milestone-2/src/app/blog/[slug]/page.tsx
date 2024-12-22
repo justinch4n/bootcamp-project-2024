8e24b0a21a35bbad8c0866bdda56df666498a51f
justin@justinchan.org <jchan258@calpoly.edu>
2024-12-21 17:14:33 -0800

    fixed awaiting issues with params and slug

--- bootcamp-milestone-2/src/app/blog/[slug]/page.tsx
+++ bootcamp-milestone-2/src/app/blog/[slug]/page.tsx
@@ -3,18 +3,31 @@ import Image from "next/image";
 import styles from "../blogPages.module.css";
 import connectDB from "@/database/db";
 import BlogModel from "@/database/blogSchema";
-import Comment from '@/components/comment';
+import Comment from "@/components/comment";
 import { IComment } from "@/database/blogSchema";
 import CommentForm from "@/components/commentForm";
 
-export default async function BlogPost({ params }: { params: { slug: string } }) {
+type BlogPostProps = {
+  params: { slug: string };
+};
+
+export default async function BlogPost({ params }: BlogPostProps) {
+  const { slug } = await params;
+
+  // Connect to the database
   await connectDB();
-  const blog = await BlogModel.findOne({ slug: params?.slug }).exec();
+
+  // Fetch the blog by slug
+  const blog = await BlogModel.findOne({ slug }).exec();
+
   if (!blog) {
     return (
       <main>
         <h1 className="pageTitle">Blog Not Found</h1>
-        <p>The blog post you are looking for does not exist. Please check the URL or go back to the <a href="/blog">blogs page</a>.</p>
+        <p>
+          The blog post you are looking for does not exist. Please check the URL
+          or go back to the <a href="/blog">blogs page</a>.
+        </p>
       </main>
     );
   }
@@ -24,8 +37,8 @@ export default async function BlogPost({ params }: { params: { slug: string } })
       <h1 className="pageTitle">{blog.title}</h1>
       <div className={styles.blogPage}>
         <div className={styles.blogImg}>
-          <Image 
-            src={blog.image || "/media/default.jpg"} // Fallback to default image if no image is set
+          <Image
+            src={blog.image || "/media/default.jpg"} // Fallback to default image
             alt={blog.imageAlt || "Default image"} // Fallback to default alt text
             width={800}
             height={600}
@@ -39,10 +52,13 @@ export default async function BlogPost({ params }: { params: { slug: string } })
         <h4 className={styles.commentTitle}>Comments:</h4>
         {blog.comments?.length > 0 ? (
           blog.comments.map((comment: IComment, idx: number) => (
-          <Comment key={idx} comment={comment} />
-        ))) : (<p>No comments yet.</p> )}
+            <Comment key={idx} comment={comment} />
+          ))
+        ) : (
+          <p>No comments yet.</p>
+        )}
       </div>
-      <CommentForm slug={params.slug} />
+      <CommentForm slug={slug} />
     </main>
   );
 }
